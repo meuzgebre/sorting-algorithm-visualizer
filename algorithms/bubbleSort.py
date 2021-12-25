443501041c87b3f1e7e949cd6811221a0726c1b6
Meuz G <95613515+meuzgebre@users.noreply.github.com>
2021-12-25 13:37:30 +0300

    Add files via upload

---
+++ algorithms/bubbleSort.py
@@ -0,0 +1,14 @@
+import time
+from colors import *
+
+def bubble_sort(data, drawData, timeTick):
+    size = len(data)
+    for i in range(size-1):
+        for j in range(size-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+                drawData(data, [PINK if x == j or x == j+1 else BLUE for x in range(len(data))] )
+                time.sleep(timeTick)
+                
+    drawData(data, [PINK for x in range(len(data))])
+  
---
+++ algorithms/insertionSort.py
@@ -0,0 +1,16 @@
+import time
+from colors import *
+
+def insertion_sort(data, drawData, timeTick):
+    for i in range(len(data)):
+        temp = data[i]
+        k = i
+        while k > 0 and temp < data[k-1]:
+            data[k] = data[k-1]
+            k -= 1
+        data[k] = temp
+        drawData(data, [PINK if x == k or x == i else BLUE for x in range(len(data))])
+        time.sleep(timeTick)
+        
+    drawData(data, [PINK for x in range(len(data))])
+
